b179943858c738758884bfe4cb01790e6cec7f0b
pavelsuk <pavelsuk@users.noreply.github.com>
2018-10-27 22:52:24 +0200

    playing with certificates from auth0.com

--- 05_secure_oauth/test/02_test_cert_token.py
+++ 05_secure_oauth/test/02_test_cert_token.py
@@ -1,12 +1,29 @@
 import jwt
 import datetime
 import time
+# from jwt.algorithms import RSAAlgorithm
+from private_config import priv_audience, priv_issuer, priv_token
+
+from cryptography.x509 import load_pem_x509_certificate
+from cryptography.hazmat.backends import default_backend
 
 with open('pubkey.pem') as f:
     PUBKEY = f.read()
 
 with open('privkey.pem') as f:
     PRIVKEY = f.read()
+
+with open('public_auth0.pem') as f:
+    PUBKEY_AUTH0 = f.read()
+
+with open('pubkey_auth0.pem', 'rb') as f:
+    certificate_text = f.read()
+    print(certificate_text)
+    # certBytes = list(certificate_text.encode())
+    # print(certBytes)
+    certificate = load_pem_x509_certificate(certificate_text, default_backend())
+    PUBKEY_AUTH0_509 = certificate.public_key()
+    print('PUBKEY_AUTH0_509: {}'.format(PUBKEY_AUTH0_509))
 
 
 def create_token(**data):
@@ -17,28 +34,62 @@ def read_token(token):
     return jwt.decode(token, PUBKEY, algorithm='RS512')
 
 
-token = create_token(some='data are', inthe='token')
-print(token)
+def read_token_256(token, pubk=PUBKEY_AUTH0, verify=True, options=None):
+    return jwt.decode(
+        token,
+        pubk,
+        algorithm='RS256',
+        verify=verify,
+        options=options,
+        audience=priv_audience,
+        issuer=priv_issuer)
 
-read = read_token(token)
-print(read)
 
-# adding expiration time to token
+def test_token():
+    token = priv_token
+    options = {
+        'verify_signature': True,
+        'verify_exp': True,
+        'verify_nbf': True,
+        'verify_iat': True,
+        'verify_aud': False,
+        'verify_iss': True,
+        'require_exp': False,
+        'require_iat': False,
+        'require_nbf': False
+    }
+    read = read_token_256(token, PUBKEY_AUTH0_509, options)
+    print(read)
 
-expire_time = datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
-token = create_token(exp=expire_time)
-print(token)
+    read = read_token_256(token)
+    print(read)
 
-time.sleep(5)
+    token = create_token(some='data are', inthe='token')
+    print(token)
 
-read = read_token(token)
-print(read)
-
-time.sleep(6)
-
-# the signature should be expired now
-try:
     read = read_token(token)
     print(read)
-except jwt.ExpiredSignatureError:
-    print('# Signature has expired')
+
+    # adding expiration time to token
+
+    expire_time = datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
+    token = create_token(exp=expire_time)
+    print(token)
+
+    time.sleep(5)
+
+    read = read_token(token)
+    print(read)
+
+    time.sleep(6)
+
+    # the signature should be expired now
+    try:
+        read = read_token(token)
+        print(read)
+    except jwt.ExpiredSignatureError:
+        print('# Signature has expired')
+
+
+if __name__ == '__main__':
+    test_token()
---
+++ 05_secure_oauth/test/private_config_example.py
@@ -0,0 +1,3 @@
+priv_audience = 'your_domain_for_audience as stated in aud parameter'
+priv_issuer = 'your_domain_for_issuer ast stated in iss parameter'
+priv_token = "private_token_generated_by_issuer"
