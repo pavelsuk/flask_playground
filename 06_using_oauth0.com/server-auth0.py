f819dc98bd01f4b3cf9944bc28a27918d730fbaa
pavelsuk <pavelsuk@users.noreply.github.com>
2018-10-28 15:15:49 +0100

    linting

--- 06_using_oauth0.com/server-auth0.py
+++ 06_using_oauth0.com/server-auth0.py
@@ -23,6 +23,7 @@ APP = Flask(__name__)
 
 # Format error response and append status code.
 class AuthError(Exception):
+
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
@@ -40,25 +41,23 @@ def get_token_auth_header():
     """
     auth = request.headers.get("Authorization", None)
     if not auth:
-        raise AuthError({"code": "authorization_header_missing",
-                        "description":
-                            "Authorization header is expected"}, 401)
+        raise AuthError({
+            "code": "authorization_header_missing",
+            "description": "Authorization header is expected"
+        }, 401)
 
     parts = auth.split()
 
     if parts[0].lower() != "bearer":
-        raise AuthError({"code": "invalid_header",
-                        "description":
-                            "Authorization header must start with"
-                            " Bearer"}, 401)
+        raise AuthError({
+            "code": "invalid_header",
+            "description": "Authorization header must start with"
+            " Bearer"
+        }, 401)
     elif len(parts) == 1:
-        raise AuthError({"code": "invalid_header",
-                        "description": "Token not found"}, 401)
+        raise AuthError({"code": "invalid_header", "description": "Token not found"}, 401)
     elif len(parts) > 2:
-        raise AuthError({"code": "invalid_header",
-                        "description":
-                            "Authorization header must be"
-                            " Bearer token"}, 401)
+        raise AuthError({"code": "invalid_header", "description": "Authorization header must be" " Bearer token"}, 401)
 
     token = parts[1]
     return token
@@ -82,33 +81,30 @@ def requires_scope(required_scope):
 def requires_auth(f):
     """Determines if the access token is valid
     """
+
     @wraps(f)
     def decorated(*args, **kwargs):
         token = get_token_auth_header()
-        jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
+        jsonurl = urlopen("https://" + AUTH0_DOMAIN + "/.well-known/jwks.json")
         jwks = json.loads(jsonurl.read())
         try:
             unverified_header = jwt.get_unverified_header(token)
         except jwt.JWTError:
-            raise AuthError({"code": "invalid_header",
-                            "description":
-                                "Invalid header. "
-                                "Use an RS256 signed JWT Access Token"}, 401)
+            raise AuthError({
+                "code": "invalid_header",
+                "description": "Invalid header. "
+                "Use an RS256 signed JWT Access Token"
+            }, 401)
         if unverified_header["alg"] == "HS256":
-            raise AuthError({"code": "invalid_header",
-                            "description":
-                                "Invalid header. "
-                                "Use an RS256 signed JWT Access Token"}, 401)
+            raise AuthError({
+                "code": "invalid_header",
+                "description": "Invalid header. "
+                "Use an RS256 signed JWT Access Token"
+            }, 401)
         rsa_key = {}
         for key in jwks["keys"]:
             if key["kid"] == unverified_header["kid"]:
-                rsa_key = {
-                    "kty": key["kty"],
-                    "kid": key["kid"],
-                    "use": key["use"],
-                    "n": key["n"],
-                    "e": key["e"]
-                }
+                rsa_key = {"kty": key["kty"], "kid": key["kid"], "use": key["use"], "n": key["n"], "e": key["e"]}
         if rsa_key:
             try:
                 payload = jwt.decode(
@@ -116,26 +112,26 @@ def requires_auth(f):
                     rsa_key,
                     algorithms=ALGORITHMS,
                     audience=API_IDENTIFIER,
-                    issuer="https://"+AUTH0_DOMAIN+"/"
-                )
+                    issuer="https://" + AUTH0_DOMAIN + "/")
             except jwt.ExpiredSignatureError:
-                raise AuthError({"code": "token_expired",
-                                "description": "token is expired"}, 401)
+                raise AuthError({"code": "token_expired", "description": "token is expired"}, 401)
             except jwt.JWTClaimsError:
-                raise AuthError({"code": "invalid_claims",
-                                "description":
-                                    "incorrect claims,"
-                                    " please check the audience and issuer"}, 401)
+                raise AuthError({
+                    "code": "invalid_claims",
+                    "description": "incorrect claims,"
+                    " please check the audience and issuer"
+                }, 401)
             except Exception:
-                raise AuthError({"code": "invalid_header",
-                                "description":
-                                    "Unable to parse authentication"
-                                    " token."}, 401)
+                raise AuthError({
+                    "code": "invalid_header",
+                    "description": "Unable to parse authentication"
+                    " token."
+                }, 401)
 
             _request_ctx_stack.top.current_user = payload
             return f(*args, **kwargs)
-        raise AuthError({"code": "invalid_header",
-                        "description": "Unable to find appropriate key"}, 401)
+        raise AuthError({"code": "invalid_header", "description": "Unable to find appropriate key"}, 401)
+
     return decorated
 
 
@@ -170,10 +166,7 @@ def private_scoped():
     if requires_scope("update:syslog"):
         response = "Hello from a private endpoint! You are authenticated and have a scope of update:syslog."
         return jsonify(message=response)
-    raise AuthError({
-        "code": "Unauthorized",
-        "description": "You don't have access to this resource"
-    }, 403)
+    raise AuthError({"code": "Unauthorized", "description": "You don't have access to this resource"}, 403)
 
 
 if __name__ == "__main__":
